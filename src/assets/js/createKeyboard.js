c7de7e5bd0b2a1c112d7303528f7e18f4b3e08d6
Vlad <makrak.vladislav@yandex.ru>
2022-05-08 19:10:04 +0300

    feat: add methods: generateKeyboard, eventHandler, changeLanguage

--- src/assets/js/createKeyboard.js
+++ src/assets/js/createKeyboard.js
@@ -1,47 +1,101 @@
 import keysData from './keysData';
+import Key from './key';
 
 class Keyboard {
-  constructor({
-    lang, code, value, shift,
-  }) {
+  constructor(lang, textArea) {
     this.lang = lang;
-    this.code = code;
-    this.value = value;
-    this.shift = shift;
+    this.textArea = textArea;
   }
 
-  createKeys() {
-    let template = '';
-    const key = document.createElement('div');
-    key.classList.add('keyboard__key');
-    template += `<button>${this.value}</button>`;
-    key.innerHTML = template;
-    key.onclick = () => {
-      console.log(this.value);
-    };
-    return key;
+  generateKeyboard() {
+    this.template = [
+      ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+      ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
+      ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+      ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+      ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+    ];
+    // this.keys = [];
+    const { body } = document;
+    const keyboardWrapper = document.createElement('div');
+    keyboardWrapper.classList.add('keyboard-wrapper');
+    body.append(keyboardWrapper);
+    const textArea = document.createElement('textarea');
+    textArea.classList.add('keyboard__input');
+    body.prepend(textArea);
+
+    this.keys = {};
+    this.template.forEach((row) => {
+      const rowEl = document.createElement('div');
+      rowEl.classList.add('keyboard__row');
+      row.forEach((keyCode) => {
+        const key = new Key(keyCode, this.lang, textArea);
+        this.keys[keyCode] = key;
+        key.keyboard = this;
+        rowEl.append(key.createKey());
+        // console.log(key);
+      });
+
+      keyboardWrapper.append(rowEl);
+    });
+
+    document.addEventListener('keydown', (event) => this.eventHandler(event));
+    // document.addEventListener('keyup', (event) => { this.eventHandler(event); });
+    // document.body.addEventListener('mousedown', (event) => { this.eventHandler(event); });
+
+    /*
+    const { body } = document;
+
+    const textArea = document.createElement('textarea');
+    textArea.classList.add('keyboard-textarea');
+    body.append(textArea);
+
+    const data = [...keysData];
+    const { lang } = this;
+
+    body.append(keyboardWrapper);
+    console.log(this.textArea);
+    data.forEach((item) => {
+      item[lang].forEach((key) => {
+        console.log(key);
+        this.keys.push(new Key(key));
+      });
+    });
+    // const createKey = new Key();
+    this.keys.forEach((item) => {
+      keyboardWrapper.append(item.createKey());
+    });
+
+    body.addEventListener('keydown', (event) => { this.eventCatcher(event); });
+    body.addEventListener('mousedown', (event) => { this.eventCatcher(event); });
+    */
+  }
+
+  eventHandler(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const keyCode = event.code;
+    const key = this.keys;
+    key[keyCode].keyDown(event);
+    if (event.ctrlKey && event.altKey) {
+      let language = localStorage.getItem('language');
+      // console.log(language);
+      if (language === 'ru') {
+        language = 'en';
+      } else {
+        language = 'ru';
+      }
+      console.log(language);
+      this.lang = language;
+      this.changeLanguage();
+    }
+  }
+
+  changeLanguage() {
+    Object.keys(this.keys).forEach((keyCode) => {
+      this.keys[keyCode].changeLanguage();
+    });
   }
 }
 
-const keys = [];
-keysData.forEach((item) => {
-  item.en.forEach((key) => {
-    keys.push(new Keyboard(key));
-  });
-});
-
-console.log(keys);
-// cards.push(new Card(item));
-
-function initKeyboard() {
-  const { body } = document;
-  const keyBoardWrapper = document.createElement('div');
-  keyBoardWrapper.classList.add('keyboard-wrapper');
-  body.append(keyBoardWrapper);
-
-  keys.forEach((item) => {
-    keyBoardWrapper.append(item.createKeys());
-  });
-}
-
-initKeyboard();
+export default Keyboard;
