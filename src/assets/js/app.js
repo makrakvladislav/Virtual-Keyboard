7f3cfad9f46d79b72915536a67efb200d7843aec
Vlad <makrak.vladislav@yandex.ru>
2022-05-08 18:45:19 +0300

    feat: save language to localstorage

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,13 @@
 import Keyboard from './createKeyboard';
 
 window.onload = () => {
-  const keyboard = new Keyboard('en');
+  let language;
+  if (localStorage.getItem('language')) {
+    language = localStorage.getItem('language');
+  } else {
+    localStorage.setItem('language', 'en');
+  }
+  const keyboard = new Keyboard(language);
   keyboard.generateKeyboard();
 
   // console.log(keys);
