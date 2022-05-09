04602fca28a93990034565ef8fc85db0af3d7487
Vlad <makrak.vladislav@yandex.ru>
2022-05-10 01:44:45 +0300

    fix: fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,13 @@ const config = {
         type: 'asset',
       },
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]',
+        },
+      },
+      {
         test: /\.(svg|ico)$/i,
         type: 'asset/resource',
         generator: {
