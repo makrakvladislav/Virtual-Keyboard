f7bcccda7cb37e0f56d0bb9d32b11b51f7e2bb9b
Vlad <makrak.vladislav@yandex.ru>
2022-05-09 05:20:15 +0300

    fix: fix control

--- src/assets/js/control.js
+++ src/assets/js/control.js
@@ -1,15 +1,22 @@
 const control = (tagName, className, content, keyCode, shiftVal) => {
+  if (className === 'key-button') {
+    const element = document.createElement(tagName);
+    element.classList.add(className);
+    const keyValue = document.createElement('span');
+    keyValue.classList.add('key-value');
+    keyValue.innerHTML = content;
+    element.append(keyValue);
+    element.value = keyCode;
+    const shiftValue = document.createElement('span');
+    shiftValue.classList.add('key-shift');
+    shiftValue.innerHTML = shiftVal;
+    element.append(shiftValue);
+    return element;
+  }
   const element = document.createElement(tagName);
   element.classList.add(className);
   element.innerHTML = content;
   element.value = keyCode;
-  if (className === 'key') {
-    const shiftValue = document.createElement('span');
-    shiftValue.classList.add('shift-value');
-    shiftValue.innerHTML = shiftVal;
-    element.append(shiftValue);
-  }
-
   return element;
 };
 
